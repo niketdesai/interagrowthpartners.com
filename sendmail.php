c6f5afdc0b9c380aeadaff6c74877da4db55fbeb
Niket <niket.de@gmail.com>
2012-06-13 15:52:39 -0700

    added more fields to contact form'

--- sendmail.php
+++ sendmail.php
@@ -2,9 +2,11 @@
   
   //  Getting the form inputs here and storing them into
   //  local variables for message creation
-  $to = 'iamniket@gmail.com';  // Target email 
+  $to = 'info@interagrowthpartners.com';  // Target email 
   $name = $_REQUEST['name'] ;
   $email = $_REQUEST['email'] ;
+  $phone = $_REQUEST['phone'] ;
+  $resume = $_REQUEST['resume'] ;
   $recruit = ($_REQUEST['recruit'] == 'on') ? 'Yep' : 'Nope' ;
   $talent = ($_REQUEST['talent'] == 'on') ? 'Yep' : 'Nope' ;
   $message = $_REQUEST['message'] ;
@@ -14,6 +16,8 @@
   //  Building the content of the email body
   $content = 'Name: '.$name."\r\n";
   $content .= 'Email: '.$email."\r\n";
+  $content .= 'Phone: '.$phone."\r\n";
+  $content .= 'Resume Link: '.$resume."\r\n";
   $content .= 'Looking for Job? '.$talent."\r\n";
   $content .= 'Looking for Talent? '.$recruit."\r\n";
 
@@ -26,9 +30,9 @@
   $headers .= 'From: '.$name.' <'.$email.'>' . "\r\n";
 
   //  Sending the email with all the related content
-  mail( $to, $subject, $content, $headers);
+  mail( $to, $subject, $content, $headers );
   
-  //  Send user to page again with request parameter as success!
+  //  Send user to page again with request parameter as success! 
   header( "Location: http://dev.witandwhim.com/intera?request=success" );
   
 ?>
